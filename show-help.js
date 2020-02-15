94d07abb2a432e2d47e6019bd173efa352991213
Andrey Sitnik <andrey@sitnik.ru>
2020-02-15 18:06:40 -0500

    Add project description to help

--- show-help.js
+++ show-help.js
@@ -6,6 +6,7 @@ let b = chalk.bold
 module.exports = function showHelp (print) {
   print(
     b('Usage: ') + 'npx print-shapshots [FILTER]',
+    'Show Jest snapshots of current project',
     '',
     b('Arguments:'),
     '  ' + y('--version') + '   Show version',
