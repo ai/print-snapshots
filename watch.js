838c0d54856916bbbec26d9e243b21b633044437
Andrey Sitnik <andrey@sitnik.ru>
2021-09-21 09:45:17 +0200

    Remove pkg-up

--- watch.js
+++ watch.js
@@ -1,10 +1,23 @@
+import { dirname, resolve, parse, join } from 'path'
 import { existsSync, readFileSync } from 'fs'
 import parseGitignore from 'parse-gitignore'
-import { dirname } from 'path'
 import chokidar from 'chokidar'
-import pkgUp from 'pkg-up'
 
 import { update } from './update.js'
+
+async function findUp(name, cwd = '') {
+  let directory = resolve(cwd)
+  let { root } = parse(directory)
+
+  while (true) {
+    let foundPath = await resolve(directory, name)
+
+    if (existsSync(foundPath)) return foundPath
+    if (directory === root) return undefined
+
+    directory = dirname(directory)
+  }
+}
 
 function throttle(fn) {
   let next, running
@@ -20,8 +33,8 @@ function throttle(fn) {
 export async function watch(print, error, cwd, filter) {
   let ignored = ['.git', 'node_modules']
 
-  let rootPath = dirname(await pkgUp())
-  let gitignorePath = `${rootPath}/.gitignore`
+  let rootPath = dirname(await findUp('package.json', cwd))
+  let gitignorePath = join(rootPath, '.gitignore')
 
   if (existsSync(gitignorePath)) {
     ignored = [
